6b21ef595c860d06b5fbb8cffaf9cedfae6ffe4c
Hepher <hepher@Mac.lan>
2026-04-05 21:34:28 -0500

    commit code

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Java Web App is running.");
+        System.out.println("Deployed via Maven to Nexus Repository Manager.");
+    }
+
+    public static String getGreeting() {
+        return "Hello from Java Web App!";
+    }
+}
